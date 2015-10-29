ff7cb91d83ea79f32eff310c3edc180e35848cc7
Luca Stefani <luca.stefani.ge1@gmail.com>
2015-10-29 10:02:38 -0700

    surnia: remove custom mkbootimg
    Change-Id: I71b2c8b9ce022d80ecce791d1fe4b8a71d719619

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,7 +38,6 @@ TARGET_CPU_VARIANT := cortex-a53
 TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
 
 #Kernel
-BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x3F ehci-hcd.park=3 vmalloc=400M utags.blkdev=/dev/block/platform/soc.0/by-name/utags utags.backup=/dev/block/platform/soc.0/by-name/utagsBackup movablecore=160M
 BOARD_KERNEL_PAGESIZE := 2048
