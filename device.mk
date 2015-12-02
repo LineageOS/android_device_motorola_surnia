50530ef0b5aecb10aaa3a52ba0b14216d4f015bb
Dan Pasanen <dan.pasanen@gmail.com>
2015-12-02 09:30:14 -0600

    surnia: Add boringssl-compat wrapper
    Change-Id: I1650b3e9e7c1de23211d6fb9c597f52b5d1867c4

--- device.mk
+++ device.mk
@@ -212,6 +212,10 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     ueventd.qcom.rc
 
+# SSL Compat
+PRODUCT_PACKAGES += \
+    libboringssl-compat
+
 # Stlport
 PRODUCT_PACKAGES += \
     libstlport
