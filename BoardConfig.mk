1b476570e9d8c9151eed265b44a50bdb5cd52401
Alberto97 <albertop2197@gmail.com>
2016-03-18 22:08:11 +0100

    surnia: Custom apns and eris for CDMA variants
    Change-Id: I819594abd7e3865333d850c51af114a395ec7ec8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,6 +24,7 @@ TARGET_KERNEL_CONFIG := surnia_defconfig
 
 # Asserts
 TARGET_OTA_ASSERT_DEVICE := XT1514,XT1521,XT1524,XT1526,XT1527,XT1523,surnia_uds,surnia_umts,surnia,surnia_udstv
+TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
 # Init
 TARGET_LIBINIT_DEFINES_FILE := $(DEVICE_PATH)/init/init_surnia.cpp
