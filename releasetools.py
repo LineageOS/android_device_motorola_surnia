eda0818796a9a242a0ce70d406a1ffcfd85280a8
Alberto97 <albertop2197@gmail.com>
2016-03-18 22:08:11 +0100

    surnia: Don't allow installing on CDMA variants
    Change-Id: I819594abd7e3865333d850c51af114a395ec7ec8

---
+++ releasetools.py
@@ -0,0 +1,74 @@
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+def IncrementalOTA_Assertions(info):
+  AddAssertion(info)
+
+def IncrementalOTA_VerifyBegin(info):
+  # Workaround for apn list changes
+  RestoreApnList(info)
+
+def IncrementalOTA_InstallEnd(info):
+  ReplaceApnList(info)
+
+def FullOTA_Assertions(info):
+  AddAssertion(info)
+
+def FullOTA_InstallEnd(info):
+  ReplaceApnList(info)
+
+
+def AddAssertion(info):
+  info.script.AppendExtra('if getprop("ro.boot.carrier") == "sprint" then')
+  info.script.AppendExtra('if getprop("ro.boot.fsg-id") == "" then')
+  info.script.AppendExtra('ui_print("Sprint CDMA variant detected!");')
+  info.script.AppendExtra('ui_print("Reboot into Bootloader Mode, connect your device to PC and from PC terminal type:");')
+  info.script.AppendExtra('ui_print("fastboot oem config fsg-id sprint   if you are a Sprint user");')
+  info.script.AppendExtra('ui_print("fastboot oem config fsg-id virgin   if you are a Virgin Mobile user");')
+  info.script.AppendExtra('ui_print("fastboot oem config fsg-id boost    if you are a Boost Mobile user");')
+  info.script.AppendExtra('abort("**********");')
+  info.script.AppendExtra('endif;')
+  info.script.AppendExtra('endif;')
+
+def ReplaceApnList(info):
+  info.script.AppendExtra('if getprop("ro.boot.radio") == "0x3" then')
+  info.script.Mount("/system")
+  info.script.Mount("/data")
+  info.script.AppendExtra('run_program("/sbin/sh", "-c", "mv /system/etc/apns-conf.xml /system/etc/apns-conf.xml.bak");')
+  info.script.AppendExtra('if getprop("ro.boot.carrier") == "sprint"')
+  info.script.AppendExtra('if getprop("ro.boot.fsg-id") == "sprint" then')
+  info.script.AppendExtra('run_program("/sbin/sh", "-c", "mv /system/etc/cdma/apns-conf-sprint.xml /system/etc/apns-conf.xml")');
+  info.script.AppendExtra('run_program("/sbin/sh", "-c", "mv /system/etc/cdma/eri-sprint.xml /data/eri.xml")');
+  info.script.AppendExtra('endif;')
+  info.script.AppendExtra('if getprop("ro.boot.fsg-id") == "virgin" then')
+  info.script.AppendExtra('run_program("/sbin/sh", "-c", "mv /system/etc/cdma/apns-conf-vmob.xml /system/etc/apns-conf.xml")');
+  info.script.AppendExtra('run_program("/sbin/sh", "-c", "mv /system/etc/cdma/eri-vmob.xml /data/eri.xml")');
+  info.script.AppendExtra('endif;')
+  info.script.AppendExtra('if getprop("ro.boot.fsg-id") == "boost" then')
+  info.script.AppendExtra('run_program("/sbin/sh", "-c", "mv /system/etc/cdma/apns-conf-boost.xml /system/etc/apns-conf.xml")');
+  info.script.AppendExtra('run_program("/sbin/sh", "-c", "mv /system/etc/cdma/eri-boost.xml /data/eri.xml")');
+  info.script.AppendExtra('endif;')
+  info.script.AppendExtra('endif;')
+  info.script.AppendExtra('ifelse(getprop("ro.boot.carrier") == "usc", run_program("/sbin/sh", "-c", "mv /system/etc/apns-conf-usc.xml /system/etc/apns-conf.xml"));)')
+  info.script.Unmount("/data")
+  info.script.Unmount("/system")
+  info.script.AppendExtra('endif;')
+
+def RestoreApnList(info):
+  info.script.AppendExtra('if getprop("ro.boot.hardware.sku") == "XT1548" then')
+  info.script.Mount("/system")
+  info.script.AppendExtra('delete("/system/etc/apns-conf.xml");')
+  info.script.AppendExtra('run_program("/sbin/sh", "-c", "mv /system/etc/apns-conf.xml.bak /system/etc/apns-conf.xml");')
+  info.script.Unmount("/system")
+  info.script.AppendExtra('endif;')
