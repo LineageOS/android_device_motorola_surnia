d478381b8f87a7f18a577d510cc577e7a66fe9dd
Alberto97 <albertop2197@gmail.com>
2016-03-22 11:08:09 +0100

    surnia: Fix CDMA network labelling after wipe data
    * We currently copy eri.xml to /data at installation time.
      CDMA users will lose proper network labelling if they
      wipe their data without flashing CM zip again.
      Fix this behavior.
    
    * Is rngplus a typo? No.
    
    Change-Id: I58a36e71b728a861619110e963cf5724f2c61597

--- releasetools.py
+++ releasetools.py
@@ -35,7 +35,7 @@ def AddAssertion(info):
   info.script.AppendExtra('ui_print("Reboot into Bootloader Mode, connect your device to PC and from PC terminal type:");')
   info.script.AppendExtra('ui_print("fastboot oem config fsg-id boost    if you are a Boost Mobile user");')
   info.script.AppendExtra('ui_print("fastboot oem config fsg-id frdm     if you are a FreedomPop user");')
-  info.script.AppendExtra('ui_print("fastboot oem config fsg-id rngplus  if you are a RingPlus user");')
+  info.script.AppendExtra('ui_print("fastboot oem config fsg-id ringplus if you are a RingPlus user");')
   info.script.AppendExtra('ui_print("fastboot oem config fsg-id sprint   if you are a Sprint user or not listed");')
   info.script.AppendExtra('ui_print("fastboot oem config fsg-id virgin   if you are a Virgin Mobile user");')
   info.script.AppendExtra('ui_print("and then flash CyanogenMod again");')
@@ -45,31 +45,29 @@ def AddAssertion(info):
 def ReplaceApnList(info):
   info.script.AppendExtra('if getprop("ro.boot.radio") == "0x3" then')
   info.script.Mount("/system")
-  info.script.Mount("/data")
   info.script.AppendExtra('run_program("/sbin/sh", "-c", "mv /system/etc/apns-conf.xml /system/etc/apns-conf.xml.bak");')
   info.script.AppendExtra('if getprop("ro.boot.carrier") == "sprint" then')
   info.script.AppendExtra('if getprop("ro.boot.fsg-id") == "boost" then')
-  info.script.AppendExtra('run_program("/sbin/sh", "-c", "mv /system/etc/cdma/apns-conf-boost.xml /system/etc/apns-conf.xml");');
-  info.script.AppendExtra('run_program("/sbin/sh", "-c", "cp /system/etc/cdma/eri-boost.xml /data/eri.xml");');
+  info.script.AppendExtra('run_program("/sbin/sh", "-c", "mv /system/etc/cdma/apns-conf-boost.xml /system/etc/apns-conf.xml");')
+  info.script.AppendExtra('run_program("/sbin/sh", "-c", "mv /system/etc/cdma/eri-boost.xml /system/etc/eri.xml");')
   info.script.AppendExtra('endif;')
   info.script.AppendExtra('if getprop("ro.boot.fsg-id") == "frdm" then')
-  info.script.AppendExtra('run_program("/sbin/sh", "-c", "mv /system/etc/cdma/apns-conf-frdm.xml /system/etc/apns-conf.xml");');
-  info.script.AppendExtra('run_program("/sbin/sh", "-c", "cp /system/etc/cdma/eri-frdm.xml /data/eri.xml");');
+  info.script.AppendExtra('run_program("/sbin/sh", "-c", "mv /system/etc/cdma/apns-conf-frdm.xml /system/etc/apns-conf.xml");')
+  info.script.AppendExtra('run_program("/sbin/sh", "-c", "mv /system/etc/cdma/eri-frdm.xml /system/etc/eri.xml");')
   info.script.AppendExtra('endif;')
-  info.script.AppendExtra('if getprop("ro.boot.fsg-id") == "rngplus" then')
-  info.script.AppendExtra('run_program("/sbin/sh", "-c", "mv /system/etc/cdma/apns-conf-rngplus.xml /system/etc/apns-conf.xml");');
-  info.script.AppendExtra('run_program("/sbin/sh", "-c", "cp /system/etc/cdma/eri-rngplus.xml /data/eri.xml");');
+  info.script.AppendExtra('if getprop("ro.boot.fsg-id") == "ringplus" || getprop("ro.boot.fsg-id") == "rngplus" then')
+  info.script.AppendExtra('run_program("/sbin/sh", "-c", "mv /system/etc/cdma/apns-conf-rngplus.xml /system/etc/apns-conf.xml");')
+  info.script.AppendExtra('run_program("/sbin/sh", "-c", "mv /system/etc/cdma/eri-rngplus.xml /system/etc/eri.xml");')
   info.script.AppendExtra('endif;')
   info.script.AppendExtra('if getprop("ro.boot.fsg-id") == "sprint" then')
-  info.script.AppendExtra('run_program("/sbin/sh", "-c", "mv /system/etc/cdma/apns-conf-sprint.xml /system/etc/apns-conf.xml");');
+  info.script.AppendExtra('run_program("/sbin/sh", "-c", "mv /system/etc/cdma/apns-conf-sprint.xml /system/etc/apns-conf.xml");')
   info.script.AppendExtra('endif;')
   info.script.AppendExtra('if getprop("ro.boot.fsg-id") == "virgin" then')
-  info.script.AppendExtra('run_program("/sbin/sh", "-c", "mv /system/etc/cdma/apns-conf-virgin.xml /system/etc/apns-conf.xml");');
-  info.script.AppendExtra('run_program("/sbin/sh", "-c", "cp /system/etc/cdma/eri-virgin.xml /data/eri.xml");');
+  info.script.AppendExtra('run_program("/sbin/sh", "-c", "mv /system/etc/cdma/apns-conf-virgin.xml /system/etc/apns-conf.xml");')
+  info.script.AppendExtra('run_program("/sbin/sh", "-c", "mv /system/etc/cdma/eri-virgin.xml /system/etc/eri.xml");')
   info.script.AppendExtra('endif;')
   info.script.AppendExtra('endif;')
   info.script.AppendExtra('ifelse(getprop("ro.boot.carrier") == "usc", run_program("/sbin/sh", "-c", "mv /system/etc/cdma/apns-conf-usc.xml /system/etc/apns-conf.xml"));')
-  info.script.Unmount("/data")
   info.script.Unmount("/system")
   info.script.AppendExtra('endif;')
 
